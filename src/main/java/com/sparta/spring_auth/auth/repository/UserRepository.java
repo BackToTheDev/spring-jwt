3d4f809ddf9c83e3ab3a7eb27426bd59ec553d55
BackToTheDev <rnjswnstjd13@gmail.com>
2025-09-15 19:04:31 +0900

    회원 가입 구현

---
+++ src/main/java/com/sparta/spring_auth/auth/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.sparta.spring_auth.auth.repository;
+
+import com.sparta.spring_auth.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
